73e21346dcd14316028ee1334be603ddb204cf2d
fern <179273869+Fern-labs@users.noreply.github.com>
2025-11-01 15:48:03 -0700

    Project: Rock Paper Scissors Playable in console.

--- jajanken.js
+++ jajanken.js
@@ -1,1 +1,70 @@
-console.log("test")
+
+function getComputerChoice(){
+    let jajanken=Math.floor(Math.random()*3)
+
+    if (jajanken === 0){
+        return "rock"
+    }else if (jajanken ===1){
+        return "paper"
+    }else{
+        return "scissors"
+    }
+
+}
+
+function getHumanChoice(){
+    let choice =prompt().toLocaleLowerCase();
+    return choice
+}
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore= 0;
+
+    function playRound(humanChoice,computerChoice){
+    
+    console.log(humanChoice)
+    console.log(computerChoice)
+    
+    if (humanChoice  === "rock" && computerChoice == "scissors"){
+        humanScore++
+        return console.log("Human Wins!")
+    }else if (humanChoice  === "paper" && computerChoice == "rock"){
+        humanScore++
+        return console.log("Human Wins!")
+    }else if (humanChoice  === "scissors" && computerChoice == "paper"){
+        humanScore++
+        return console.log("Human Wins!")
+    }else if(humanChoice=== computerChoice){
+        return console.log("Tie")
+    }
+    else{
+        computerScore++
+        return console.log("Clanker Wins")
+    }
+
+}
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(humanScore);
+    console.log(computerScore);
+}
+
+playGame();
+
+
+
+
